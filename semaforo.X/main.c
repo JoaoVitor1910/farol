3f127ce97e8e035589fe6bcdf9dc2f36ca461452
21192696 <21192696@DSN1271158638.cfp127aluno.local>
2021-02-19 15:33:47 -0200

    inclusao de arquivos de configuracao, e arquivo principal main.c e semaforo.c

---
+++ semaforo.X/main.c
@@ -0,0 +1,20 @@
+/*
+ * File:   main.c
+ * Author: 21192696
+ *
+ * Created on 19 de Fevereiro de 2021, 14:49
+ */
+
+
+#include <xc.h>
+#include "config.h"
+#include "semaforo.h"
+
+void main(void) 
+{
+    while(1)
+    {
+        
+    }
+}
+ 
---
+++ semaforo.X/semaforo.c
@@ -0,0 +1,14 @@
+/*
+ * File:   semaforo.c
+ * Author: 21192696
+ *
+ * Created on 19 de Fevereiro de 2021, 15:08
+ */
+
+
+#include <xc.h>
+
+void semaforo_init (void)
+{
+    
+}
